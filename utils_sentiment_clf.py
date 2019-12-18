ddfc4b94d669080f2be94a51d9f5ea67508f98c5
HuManChu <philipp.ausserlechner@gmail.com>
2019-12-18 19:24:49 +0100

    last changes

--- utils_sentiment_clf.py
+++ utils_sentiment_clf.py
@@ -12,12 +12,12 @@ from sklearn.metrics import classification_report
 
 
 TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
-stop_words = None
-stemmer = None
-
-source = "https://www.kaggle.com/maroberti/fastai-with-transformers-bert-roberta"
 
 
+'''
+The seed_all method sets the seed in all necessary classes to get reproducable results.
+Taken from: https://www.kaggle.com/maroberti/fastai-with-transformers-bert-roberta
+'''
 def seed_all(seed_value):
     rnd.seed(seed_value)  # Python
     np.random.seed(seed_value)  # cpu vars
@@ -29,7 +29,10 @@ def seed_all(seed_value):
         torch.backends.cudnn.deterministic = True  # needed
         torch.backends.cudnn.benchmark = False
 
-
+'''
+The load_and_process_data function takes the path to the data, the encoding and the column names of the data and loads it.
+Afterwards we drop the NAs, rename the columns, clean the data and set label 4 to 1, which is more common.
+'''
 def load_and_process_data(file: str, label: str, text: str, encoding: str, column_names: str, random_state = 123, test_size=0.2):
     df = pd.read_csv(file,encoding = encoding, names = column_names)
     df = df.dropna()
@@ -47,7 +50,10 @@ def _preprocess_txt(text):
     text = re.sub(TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
     return text
 
-
+'''
+The get_classification_report function calculates the model predictions for the test set and calculates the precision, recall and
+accuracy.
+'''
 def get_classification_report(learner,test,data):
     """
     the get_preds method does not yield the elements in order by default
